d26045eb728d7a10e17e133a4196710959133196
christopher manouana <irvinemanouana@users.noreply.github.com>
2025-02-13 11:03:47 +0100

    init project

---
+++ app/src/main/java/com/marshall/therickandmorty/Application.kt
@@ -0,0 +1,17 @@
+package com.marshall.therickandmorty
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class Application: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@Application)
+        }
+    }
+}
